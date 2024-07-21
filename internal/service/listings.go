1bc59282dced9a27d6c189809525a3db5dac46f3
feuvpi <fredvpg@gmail.com>
2024-07-21 12:55:02 -0300

    feat listing service

--- internal/service/listings.go
+++ internal/service/listings.go
@@ -15,4 +15,34 @@ func NewListingsService(db *database.ListingsDatabase) *ListingsService {
 	return &ListingsService{db: db}
 }
 
+func (s *ListingsService) GetAllListings() ([]models.Listing, error) {
+	return s.db.GetAll()
+}
 
+func (s *ListingsService) GetListingByID(id string) (*models.Listing, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	return s.db.GetByID(objectID)
+}
+
+func (s *ListingsService) CreateListing(listing models.Listing) (interface{}, error) {
+	return s.db.Insert(listing)
+}
+
+func (s *ListingsService) UpdateListing(id string, listing models.Listing) (int64, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, err
+	}
+	return s.db.Update(objectID, listing)
+}
+
+func (s *ListingsService) DeleteListing(id string) (int64, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, err
+	}
+	return s.db.Delete(objectID)
+}
