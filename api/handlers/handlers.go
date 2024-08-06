4ca4106c7a9b04701c0db132971aba7806c8ea1d
feuvpi <fredvpg@gmail.com>
2024-08-05 23:47:49 -0300

    .

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -17,7 +17,43 @@ import (
 var listingsService *services.ListingsService
 var usersService *services.UsersService
 
+type QueryParams struct {
+    Page     int `validate:"min=1"`
+    PageSize int `validate:"min=1,max=100"`
+}
+
+func getPaginatedListings(w http.ResponseWriter, r *http.Request) {
+	params := QueryParams{
+        Page:     r.URL.Query().Get("page"),
+        PageSize: r.URL.Query().Get("pageSize"),
+    }
+	
+	if err := validator.New().Struct(params); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	page, _ := strconv.Atoi(params.Page)
+    pageSize, _ := strconv.Atoi(params.PageSize)
+
+	listings, err := listingsService.GetPaginatedListings(page, pageSize)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, "No listings found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Error fetching listings", http.StatusInternalServerError)
+		}
+		return
+	}
+	json.NewEncoder(w).Encode(listings)
+}
+
 func getListings(w http.ResponseWriter, r *http.Request) {
+	params := QueryParams{
+        Page:     r.URL.Query().Get("page"),
+        PageSize: r.URL.Query().Get("pageSize"),
+    }
+
 	listings, err := listingsService.GetAllListings()
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
